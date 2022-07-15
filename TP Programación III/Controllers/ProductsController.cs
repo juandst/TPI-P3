3937f7b85bf2594ff60296dd6343c261f2ef33c3
juan-ds <juanods1@gmail.com>
2022-07-15 19:25:09 -0300

    Agregar inyección de dependencia y método GET

--- TP Programación III/Controllers/ProductsController.cs
+++ TP Programación III/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using TP_Programación_III.Data;
 using TP_Programación_III.Models;
+using TP_Programación_III.Services;
 
 namespace TP_Programación_III.Controllers
 {
@@ -9,13 +10,17 @@ namespace TP_Programación_III.Controllers
     [Route("products/")]
     public class ProductsController : ControllerBase
     {
-
+        private readonly IProductRepository _repository;
+        public ProductsController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
 
         [HttpGet]
 
-        public ActionResult<IEnumerable<Product>> GetProducts()
+        public JsonResult GetProducts()
         {
+            return new JsonResult(_repository.GetProducts());
         }
-
     }
 }
