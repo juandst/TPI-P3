f56f53305a494a85ae77e94a9a8b7924792ea2ce
juan-ds <juanods1@gmail.com>
2022-07-15 19:27:09 -0300

    Agregar configuración e inyecciones de dependencia

--- TP Programación III/Program.cs
+++ TP Programación III/Program.cs
@@ -1,4 +1,6 @@
-using TP_Programación_III.Data;
+using Microsoft.EntityFrameworkCore;
+using TP_Programación_III.Data.Context;
+using TP_Programación_III.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<Context>(dbContextOptions => dbContextOptions.UseSqlite(
+    builder.Configuration["ConnectionStrings:ContextDBConnectionString"]
+    ));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
