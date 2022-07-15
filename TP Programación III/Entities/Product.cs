cd9f2d47834e28c40afcc9569a168ab281a84468
juan-ds <juanods1@gmail.com>
2022-07-15 19:25:44 -0300

    Agregar entidades

---
+++ TP Programación III/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TP_Programación_III.Enums;
+
+namespace TP_Programación_III.Entities
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(300)]
+        public string Description { get; set; }
+
+        public Category ProductCategory { get; set; }
+
+        public int Price { get; set; }
+
+        public bool OnSale { get; set; }
+    }
+}
---
+++ TP Programación III/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TP_Programación_III.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+
+
+    }
+}
