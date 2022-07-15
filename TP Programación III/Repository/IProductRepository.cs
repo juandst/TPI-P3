290dbad7c295e106873944766b0caa9c5df8e4ee
juan-ds <juanods1@gmail.com>
2022-07-15 19:26:14 -0300

    Agregar patrón repository

---
+++ TP Programación III/Repository/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using TP_Programación_III.Entities;
+
+namespace TP_Programación_III.Services
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetProducts();
+        public Product GetProductById(int id);
+        public Product GetProductByName(string name);
+    }
+}
---
+++ TP Programación III/Repository/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using TP_Programación_III.Data.Context;
+using TP_Programación_III.Entities;
+
+namespace TP_Programación_III.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+        public ProductRepository(Context context)
+        {
+            _context = context;
+        }
+        public Product GetProductById(int id)
+        {
+            return _context.Products.Where(p => p.ID == id).FirstOrDefault();
+        }
+
+        public Product GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.OrderBy(p => p.Name).ToList(); 
+        }
+    }
+}
