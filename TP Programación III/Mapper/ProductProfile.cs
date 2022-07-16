c9325bcd7ac3f4744c08ba5658c972ed66078a47
juan-ds <juanods1@gmail.com>
2022-07-16 03:40:01 -0300

    Agregar mapper y perfiles

---
+++ TP Programación III/Mapper/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TP_Programación_III.Entities;
+using TP_Programación_III.Models;
+
+namespace TP_Programación_III.Mapper
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDTO>();
+        }
+    }
+}
