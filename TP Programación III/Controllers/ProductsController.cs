dd588f8889e6c7a4e9df1355197f23dcf8a830e1
juan-ds <juanods1@gmail.com>
2022-07-16 03:38:43 -0300

    Agregar busqueda por id - nombre - en oferta y mapper

--- TP Programación III/Controllers/ProductsController.cs
+++ TP Programación III/Controllers/ProductsController.cs
@@ -1,26 +1,55 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TP_Programación_III.Data;
 using TP_Programación_III.Models;
-using TP_Programación_III.Services;
+using TP_Programación_III.Repository;
 
 namespace TP_Programación_III.Controllers
 {
     [ApiController]
-    [Route("products/")]
+    [Route("api/[controller]/[action]")]
     public class ProductsController : ControllerBase
     {
         private readonly IProductRepository _repository;
-        public ProductsController(IProductRepository repository)
+        private readonly IMapper _mapper;
+        public ProductsController(IProductRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet]
 
-        public JsonResult GetProducts()
+        public ActionResult<IEnumerable<ProductDTO>> GetProducts()
         {
-            return new JsonResult(_repository.GetProducts());
+            var products = _repository.GetProducts();
+            return Ok(_mapper.Map<IEnumerable<ProductDTO>>(products));
         }
+
+        [HttpGet("{idProduct}")]
+        
+        public ActionResult<ProductDTO> GetProductById(int idProduct)
+        {
+            var product = _repository.GetProductById(idProduct);
+            if (product == null) return NotFound();
+            return Ok(_mapper.Map<ProductDTO>(product));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductDTO>> GetProductOnSale()
+        {
+            var productOnSale = _repository.GetProductsOnSale();
+            if (productOnSale == null) return NotFound();
+            return Ok(_mapper.Map<IEnumerable<ProductDTO>>(productOnSale));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductDTO>> GetProductByName(string name)
+        {
+            var products = _repository.GetProductByName(name);
+            if (products == null) return NotFound();
+            return Ok(_mapper.Map<IEnumerable<ProductDTO>>(products));
+    }
     }
 }
