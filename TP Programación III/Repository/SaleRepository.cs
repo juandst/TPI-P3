d16c0405c12344a5f193721ed804a0b0ed528d27
juan-ds <juanods1@gmail.com>
2022-07-17 02:53:13 -0300

    Agregar método para GET

---
+++ TP Programación III/Repository/SaleRepository.cs
@@ -0,0 +1,21 @@
+﻿using TP_Programación_III.Data.Context;
+using TP_Programación_III.Entities;
+
+namespace TP_Programación_III.Repository
+{
+    public class SaleRepository : ISaleRepository
+    {
+        private readonly Context _context;
+
+        public SaleRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Sale> GetSales()
+        {
+            return _context.Sales.ToList();
+        }
+
+    }
+}
