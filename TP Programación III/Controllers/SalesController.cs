f0fd73b2ebcc24c89395fd66b35ed82b2cacf95d
juan-ds <juanods1@gmail.com>
2022-07-17 02:07:41 -0300

    Agregar controlador

---
+++ TP Programación III/Controllers/SalesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP_Programación_III.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class SalesController : ControllerBase
+    {
+    }
+}
