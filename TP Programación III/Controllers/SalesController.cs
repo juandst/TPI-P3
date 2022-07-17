2c437d444ca6918abeb3e0228be407827b63a69e
juan-ds <juanods1@gmail.com>
2022-07-17 02:52:35 -0300

    Agregar método GET

--- TP Programación III/Controllers/SalesController.cs
+++ TP Programación III/Controllers/SalesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TP_Programación_III.Models;
+using TP_Programación_III.Repository;
 
 namespace TP_Programación_III.Controllers
 {
@@ -6,5 +9,19 @@ namespace TP_Programación_III.Controllers
     [Route("api/[controller]/[action]")]
     public class SalesController : ControllerBase
     {
+        private readonly ISaleRepository _repository;
+        private readonly IMapper _mapper;
+        public SalesController(ISaleRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<SaleDTO>> GetSales()
+        {
+            var sales = _repository.GetSales();
+            return Ok(_mapper.Map<IEnumerable<SaleDTO>>(sales));
+        }
     }
 }
