8e772e8a5a103f63101e1a8239d798a2f56c733f
juan-ds <juanods1@gmail.com>
2022-07-17 02:53:01 -0300

    Agregar método para GET

---
+++ TP Programación III/Repository/ISaleRepository.cs
@@ -0,0 +1,9 @@
+﻿using TP_Programación_III.Entities;
+
+namespace TP_Programación_III.Repository
+{
+    public interface ISaleRepository
+    {
+        public IEnumerable<Sale> GetSales();
+    }
+}
