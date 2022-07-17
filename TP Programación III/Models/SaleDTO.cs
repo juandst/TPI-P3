fd7e31f82bb696583afaef601656449ebbf9c9a1
juan-ds <juanods1@gmail.com>
2022-07-17 02:09:25 -0300

    Agregar DTO de venta

---
+++ TP Programación III/Models/SaleDTO.cs
@@ -0,0 +1,14 @@
+﻿using TP_Programación_III.Enums;
+
+namespace TP_Programación_III.Models
+{
+    public class SaleDTO
+    {
+        public int ID { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public string ShippingPlace { get; set; }
+        public string Name { get; set; }
+        public Category ProductCategory { get; set; }
+        public int Price { get; set; }
+    }
+}
