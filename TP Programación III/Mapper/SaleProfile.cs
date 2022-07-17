f7e40292a8a3fc10d397dd3ca6635a8ae1a31945
juan-ds <juanods1@gmail.com>
2022-07-17 02:52:47 -0300

    Agregar perfil para mapear

---
+++ TP Programación III/Mapper/SaleProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TP_Programación_III.Entities;
+using TP_Programación_III.Models;
+
+namespace TP_Programación_III.Mapper
+{
+    public class SaleProfile : Profile
+    {
+        public SaleProfile()
+        {
+            CreateMap<Sale, SaleDTO>();
+        }
+    }
+}
