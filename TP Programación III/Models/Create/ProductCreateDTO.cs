53d07fba12df438a5c08ce500a0ab8a42a11d801
juan-ds <juanods1@gmail.com>
2022-07-18 14:20:03 -0300

    Agregar DTO de creación

---
+++ TP Programación III/Models/Create/ProductCreateDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TP_Programación_III.Enums;
+
+namespace TP_Programación_III.Models.Create
+{
+    public class ProductCreateDTO
+    {
+        [MaxLength(50, ErrorMessage = "Nombre muy largo")]
+        [MinLength(5, ErrorMessage="Nombre muy corto")]
+        [Required(ErrorMessage = "Agregá un nombre")]
+        public string Name { get; set; }
+
+        [MaxLength(300, ErrorMessage ="Descripción muy larga")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Agregá una categoria")]
+        [Range(0, 3)]
+        public Category ProductCategory { get; set; }
+
+        [Required(ErrorMessage = "Agregá un precio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Precio inválido")]
+        public int Price { get; set; }
+        [Required]
+        public bool HasDiscount { get; set; }
+
+    }
+}
