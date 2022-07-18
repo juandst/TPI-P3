bcb59d00a7528b9430b839e50d377de43907dac4
juan-ds <juanods1@gmail.com>
2022-07-18 14:19:53 -0300

    Agregar mapeos

--- TP Programación III/Mapper/ProductProfile.cs
+++ TP Programación III/Mapper/ProductProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using TP_Programación_III.Entities;
 using TP_Programación_III.Models;
+using TP_Programación_III.Models.Create;
+using TP_Programación_III.Models.Update;
 
 namespace TP_Programación_III.Mapper
 {
@@ -9,6 +11,8 @@ namespace TP_Programación_III.Mapper
         public ProductProfile()
         {
             CreateMap<Product, ProductDTO>();
+            CreateMap<ProductCreateDTO, Product>();
+            CreateMap<ProductUpdateDTO, Product>();
         }
     }
 }
