cf174d0b79a52a6bb70f44a6fbc69b218f3c7389
juan-ds <juanods1@gmail.com>
2022-07-18 14:20:14 -0300

    Agregar DTO de actualización

---
+++ TP Programación III/Models/Update/ProductUpdateDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TP_Programación_III.Enums;
+
+namespace TP_Programación_III.Models.Update
+{
+    public class ProductUpdateDTO
+    {
+        [MaxLength(50, ErrorMessage = "Nombre muy largo")]
+        [MinLength(5, ErrorMessage = "Nombre muy corto")]
+        [Required(ErrorMessage = "Agregá un nombre")]
+        public string Name { get; set; }
+
+        [MaxLength(300, ErrorMessage = "Descripción muy larga")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Agregá una categoria")]
+        [Range(0, 3)]
+        public Category ProductCategory { get; set; }
+
+        [Required(ErrorMessage = "Agregá un precio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Precio inválido")]
+        public int Price { get; set; }
+        [Required]
+        public bool HasDiscount { get; set; }
+    }
+}
