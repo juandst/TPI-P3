56b322a5c93a2625dd74c301bb3d72056184f096
juan-ds <juanods1@gmail.com>
2022-07-19 18:31:40 -0300

    Agregar métodos para realizar validación de usuario

--- TP Programación III/Repository/IUserRepository.cs
+++ TP Programación III/Repository/IUserRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace TP_Programación_III.Repository
+﻿using TP_Programación_III.Auth;
+using TP_Programación_III.Entities;
+
+namespace TP_Programación_III.Repository
 {
     public interface IUserRepository
     {
+        User? ValidateUser(AuthRequestBody authenticationRequest);
     }
 }
--- TP Programación III/Repository/UserRepository.cs
+++ TP Programación III/Repository/UserRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace TP_Programación_III.Repository
+﻿using TP_Programación_III.Auth;
+using TP_Programación_III.Data.Context;
+using TP_Programación_III.Entities;
+
+namespace TP_Programación_III.Repository
 {
     public class UserRepository : IUserRepository
     {
+        private readonly Context _context;
+        public UserRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public User? ValidateUser(AuthRequestBody request)
+        {
+
+            var user = _context.Users.FirstOrDefault(a => a.Email == request.Email && a.Password == request.Password);
+            return user;
+        }
     }
 }
