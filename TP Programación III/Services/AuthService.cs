40a89eea45e1d1299c84a11d6a4498baeb3b31b1
juan-ds <juanods1@gmail.com>
2022-07-19 18:31:51 -0300

    Agregar servicios

---
+++ TP Programación III/Services/AuthService.cs
@@ -0,0 +1,25 @@
+﻿using TP_Programación_III.Auth;
+using TP_Programación_III.Entities;
+using TP_Programación_III.Repository;
+
+namespace TP_Programación_III.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AuthService(IUserRepository usuarioRepository)
+        {
+            _userRepository = usuarioRepository;
+        }
+
+        public User? AuthUser(AuthRequestBody authenticationRequest)
+        {
+            if (String.IsNullOrEmpty(authenticationRequest.Email) || String.IsNullOrEmpty(authenticationRequest.Password))
+            {
+                return null;
+            }
+            return _userRepository.ValidateUser(authenticationRequest);
+        }
+    }
+}
---
+++ TP Programación III/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using TP_Programación_III.Auth;
+using TP_Programación_III.Entities;
+
+namespace TP_Programación_III.Services
+{
+    public interface IAuthService
+    {
+        public User? AuthUser(AuthRequestBody authenticationRequest);
+    }
+}
